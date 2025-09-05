9968bbeb62f49cb2a6b2b752b4292bb8b23abe34
PutraGandaD <putraganda.dewata@gmail.com>
2025-09-05 08:36:05 +0700

    Add basic reusable list Compose UI component for future usage

---
+++ app/src/main/java/com/putragandad/basicnavigationcompose/ui/component/SearchList.kt
@@ -0,0 +1,81 @@
+package com.putragandad.basicnavigationcompose.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.putragandad.basicnavigationcompose.R
+import com.putragandad.basicnavigationcompose.screens.foryou.ForYouScreen
+import com.putragandad.basicnavigationcompose.ui.theme.BasicNavigationComposeTheme
+
+@Composable
+fun SearchList(
+    author: String,
+    shortDescription: String,
+    image: Painter,
+    onClickPress: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClickPress),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = "Profile Picture of $author",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f) // use the remaining space
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = author,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = shortDescription,
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchListComponentPreview() {
+    BasicNavigationComposeTheme {
+        SearchList(
+            author ="Author 1",
+            shortDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            image = painterResource(id = R.drawable.ic_blank_profile),
+            onClickPress = {}
+        )
+    }
+}
